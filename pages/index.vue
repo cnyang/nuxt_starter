9e7e6c512044978362b288ef312b5cd6382414c9
cnyang <cheinnn@gmail.com>
2021-08-17 13:09:50 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center"> </v-card>
+      <v-card>
+        <v-card-title class="headline"> this is index </v-card-title>
+        <v-card-text>
+          <p>this is a starter template</p>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
